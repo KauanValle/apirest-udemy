5db6a31ca86193adda248c3f19c7e61f026995c6
KauanValle <kauanvalle3@gmail.com>
2021-12-12 13:02:56 -0300

    Criado o crud básico da API

---
+++ app/Http/Controllers/Api/ProductController.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    /**
+     * @var Product
+     */
+    private $product;
+
+    public function __construct(Product $product)
+    {
+        $this->product = $product;
+    }
+
+
+    public function index()
+    {
+        $products = $this->product->all();
+
+        return response()->json($products);
+    }
+
+    public function get($id)
+    {
+        $product = $this->product->find($id);
+
+        return response()->json($product);
+    }
+
+    public function save(Request $request)
+    {
+        $data = $request->all();
+
+        $product = $this->product->create($data);
+
+        return response()->json($product);
+    }
+
+    public function update(Request $request, Product $id)
+    {
+
+        $id->name = $request->name;
+        $id->price = $request->price;
+        $id->description = $request->description;
+        $id->slug = $request->slug;
+        $id->save();
+
+        return response()->json($id);
+    }
+
+    public function delete($id)
+    {
+        $product = $this->product->find($id);
+        $product->delete();
+
+        return response()->json([
+            'data' => [
+                'msg' => 'Produto removido com sucesso'
+            ]
+        ]);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,13 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::namespace('Api')->group(function () {
+    Route::get('/products', 'ProductController@index');
+    Route::get('/products/{id}', 'ProductController@get');
+    Route::post('/products', 'ProductController@save');
+    Route::put('/products/{id}', 'ProductController@update');
+    Route::delete('/products/{id}', 'ProductController@delete');
+});
+
+
